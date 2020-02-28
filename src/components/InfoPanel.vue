5f299407d4c2e1128fe5991c6ac7628b1af32dd3
bogay <pojay11523@gmail.com>
2020-02-28 11:28:35 +0800

    Again...

---
+++ src/components/InfoPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card>
+    <v-card-text></v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'InfoPanel'
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-treeview hoverable :items="items"></v-treeview>
+</template>
+
+<script>
+class Item {
+  constructor(id, name, children) {
+    this.id = id
+    this.name = name
+    this.children = children || []
+  }
+}
+
+export default {
+  name: 'SideBar',
+  data() {
+    return {
+      items: []
+    }
+  },
+  created() {
+    this.items = this.getItems();
+  },
+  methods: {
+    getItems() {
+      return [
+        new Item(0, 'Item #0', [new Item(1, 'item 00'), new Item(2, 'item 01'), new Item(3, 'item 02'),])
+      ]
+    },
+  }
+}
+</script>
+
+<style>
+</style>
