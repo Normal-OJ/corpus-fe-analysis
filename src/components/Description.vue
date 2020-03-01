dce97c9c83749e10e0f337ef83f7efc8f6874631
bogay <pojay11523@gmail.com>
2020-03-01 10:33:59 +0800

    Add `Description` component

---
+++ src/components/Description.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>提供者：{{ desc.provider}}</span>
+    <div v-for="(v, i) in desc.introduction" :key="i">{{ v }}</div>
+    <div>引用資訊</div>
+    <div>{{ desc.quoteInfo }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Description',
+  props: {
+    desc: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+</style>
