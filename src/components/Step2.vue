650e859dd791705ccc4faf9a4dddb05c3970bbeb
Uier <ap9940506@gmail.com>
2020-03-07 03:31:31 +0800

    修正下載 更新表格(有效筆數)

--- src/components/Step2.vue
+++ src/components/Step2.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-container fluid style="min-height: 100vh; padding: 0 15vw;">
+  <v-container fluid style="min-height: 100vh; padding: 0 10vw;">
 
     <v-row>
 
-      <v-col cols="8">
+      <v-col cols="12" md="8">
         <v-data-table
           v-if="items"
           :headers="headers"
@@ -14,13 +14,13 @@
       </v-col>
 
 
-      <v-col cols="4">
+      <v-col cols="12" md="4">
         <v-row justify="center" class="my-6">
           <v-btn
             width="80%"
             color="info"
             :loading="!filename"
-            :href="`http://noj.tw:8777/api/download?file=${filename}`"
+            @click="download"
           ><v-icon>mdi-download</v-icon>下載分析原始資料</v-btn>
         </v-row>
         <v-row justify="center" class="my-6">
@@ -47,7 +47,7 @@
         <v-card-title></v-card-title>
         <v-card-text class="text-center text--primary">
           <v-icon color="warning" size="5rem">mdi-alert-circle-outline</v-icon>
-          <p class="headline mt-3">確定要回到重新分析頁面嗎？</p>
+          <p class="headline mt-3">確定要重新分析嗎？</p>
           <p class="subtitle-1">您目前的分析資料將不會被保留。</p>
         </v-card-text>
         <v-card-actions class="pb-12">
@@ -88,6 +88,7 @@ export default {
             'name': this.indicator[key],
             'avg': value[0],
             'sd': value[1],
+            'size': value[2],
           });
         }
         return temp;
@@ -105,14 +106,50 @@ export default {
           align: 'start',
           value: 'name', 
           class: 'text--primary subtitle-1 font-weight-bold',
+          width: '30%',
+          sortable: false,
         },
-        { text: '平均', value: 'avg', class: 'text--primary subtitle-1 font-weight-bold' },
-        { text: '標準差', value: 'sd', class: 'text--primary subtitle-1 font-weight-bold' },
+        { text: '平均', value: 'avg', class: 'text--primary subtitle-1 font-weight-bold', width: '25%' },
+        { text: '標準差', value: 'sd', class: 'text--primary subtitle-1 font-weight-bold', width: '25%' },
+        { text: '有效筆數', value: 'size', class: 'text--primary subtitle-1 font-weight-bold', width: '20%' },
       ],
       indicator: json.reindicator,
+      cache: null,
     }
   },
 
+  methods: {
+    download() {
+      if ( this.cache ) {
+        var file = new Blob([this.cache], { type: 'text/plain;charset=utf-8' });
+        if (window.navigator.msSaveOrOpenBlob) { // IE10+
+          window.navigator.msSaveOrOpenBlob(file, 'report.xls');
+        }
+        else { // Others
+          var a = document.createElement("a");
+          var url = URL.createObjectURL(file);
+          a.href = url;
+          a.download = 'report.xls';
+          document.body.appendChild(a);
+          a.click();
+          setTimeout(function () {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+          }, 0);
+        }
+      } else {
+        this.$http.get(`/api/download?file=${this.filename}`)
+          .then((res) => {
+            this.cache = res.data;
+            this.download();
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      }
+    }
+
+  }
 }
 </script>
 
