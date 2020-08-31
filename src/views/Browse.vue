788769a3b96540f390773e61c56929b096a5c7c7
bogay <pojay11523@gmail.com>
2020-08-31 22:30:53 +0800

    Change event name to kebab case

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -11,12 +11,9 @@
         ></SideBar>
       </v-col>
       <v-col cols="9">
-        <InfoPanel :file="file" @analysisFile="analysisFile">
+        <InfoPanel :file="file" @analysis-file="analysisFile">
           <div v-if="!content.provider">
-            <div
-              class="subtitle-1"
-              style="white-space: pre;"
-            >{{ content }}</div>
+            <div class="subtitle-1" style="white-space: pre;">{{ content }}</div>
           </div>
           <div v-else>
             <Description :desc="content"></Description>
@@ -25,30 +22,28 @@
       </v-col>
     </v-row>
 
-    <UiAlert 
-      v-model="alert" 
+    <UiAlert
+      v-model="alert"
       title="確定要分析此目錄底下所有檔案嗎？"
-      subtitle="確認後將前往分析頁面。" 
+      subtitle="確認後將前往分析頁面。"
       @ok="analysisFolder(file)"
     ></UiAlert>
 
-    <v-snackbar v-model="snackbar" color="error">
-      無法取得檔案
-    </v-snackbar>
+    <v-snackbar v-model="snackbar" color="error">無法取得檔案</v-snackbar>
   </v-container>
 </template>
 
 <script>
-import SideBar from '@/components/SideBar'
-import InfoPanel from '@/components/InfoPanel'
-import Description from '@/components/Description'
-import File from '@/util/file'
-import UiAlert from '@/components/ui-alert'
+import SideBar from "@/components/SideBar";
+import InfoPanel from "@/components/InfoPanel";
+import Description from "@/components/Description";
+import File from "@/util/file";
+import UiAlert from "@/components/ui-alert";
 
-const ROOT_FILE_NAME = '_I_ROOT_FILE__'
+const ROOT_FILE_NAME = "_I_ROOT_FILE__";
 
 export default {
-  name: 'Browse',
+  name: "Browse",
   components: {
     SideBar,
     InfoPanel,
@@ -58,19 +53,19 @@ export default {
   data() {
     return {
       items: [],
-      content: '',
-      fileName: '',
+      content: "",
+      fileName: "",
       opend: [],
       active: [],
-      file: new File(''),
+      file: new File(""),
       snackbar: false,
       alert: false,
-    }
+    };
   },
   async created() {
-    let root = new File(ROOT_FILE_NAME)
+    let root = new File(ROOT_FILE_NAME);
     await this.getItems(root);
-    this.items = root.children
+    this.items = root.children;
   },
   methods: {
     analysisFolder(fileName) {
@@ -78,80 +73,85 @@ export default {
       this.analysisFile(fileName);
     },
     analysisFile(fileName) {
-      if ( fileName ) {
-        this.$router.push({name: 'analysis', params: {file: fileName.fullName}});
+      if (fileName) {
+        this.$router.push({
+          name: "analysis",
+          params: { file: fileName.fullName },
+        });
       } else {
         this.snackbar = true;
       }
     },
     getItemByPath(path) {
-      path = path.split('/').splice(-1, 1)
-      let ret = this.items
+      path = path.split("/").splice(-1, 1);
+      let ret = this.items;
       for (let p of path) {
         for (let f of ret) {
           if (f.name === p) {
-            ret = f
-            break
+            ret = f;
+            break;
           }
         }
       }
-      return ret
+      return ret;
     },
     showDesc(fileName) {
-      let it = this.getItemByPath(fileName)
-      this.active = []
-      this.content = it.data
-      this.file = it
+      let it = this.getItemByPath(fileName);
+      this.active = [];
+      this.content = it.data;
+      this.file = it;
     },
     clickFile(files) {
       if (!files.length) return;
-      let f = files[0]
-      this.file = f
-      if ( f.selectAll ) {
-        this.alert = true
+      let f = files[0];
+      this.file = f;
+      if (f.selectAll) {
+        this.alert = true;
         return;
       }
-      this.content = f.data
+      this.content = f.data;
     },
     async getItems(file) {
       try {
-        let resp = await this.$http.get('/api/view', { params: { file: file.fullName || '/' } })
-        var data = resp.data;
+        let data = (
+          await this.$http.get("/api/view", {
+            params: { file: file.fullName || "/" },
+          })
+        ).data;
         // parse info panel
-        if (!!data.description) { // colletion
-          file.data = JSON.parse(data.description)
+        if (!!data.description) {
+          // colletion
+          file.data = JSON.parse(data.description);
           // file.data = JSON.parse('{"provider": "Bogay Chuang", "introduction": ["i am Bogay"], "quoteInfo": ":P"}')
-        }
-        else if (!!data.content) { // file
+        } else if (!!data.content) {
+          // file
           // file.data = data.content.split('\n')
-          file.data = data.content
+          file.data = data.content;
         }
         if (file.children !== null) {
           // parse tree view
           let isFolder = false;
           if (data.folders?.length) {
-            data.folders.forEach(f => file.pushDir(f))
+            data.folders.forEach((f) => file.pushDir(f));
             isFolder = true;
           }
           if (data.files?.length) {
-            data.files.forEach(f => file.pushFile(f))
+            data.files.forEach((f) => file.pushFile(f));
             isFolder = true;
           }
           // get children
-          for (let c of file.children)
-            this.getItems(c)
+          for (let c of file.children) this.getItems(c);
           // prepend "select all" item
-          if ( isFolder ) {
-            file.pushNode('分析此目錄所有檔案')
+          if (isFolder) {
+            file.pushNode("分析此目錄所有檔案");
           }
         }
-      }
-      catch (err) {
-        console.log(err)
+      } catch (err) {
+        console.log(err);
       }
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="css" scoped>
