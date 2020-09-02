ec8c088152e20c4c792e3168070cccd1047b2c17
bogay <pojay11523@gmail.com>
2020-09-02 10:51:57 +0800

    Seperate data table from `AnalysisResult`

--- src/components/AnalysisResult.vue
+++ src/components/AnalysisResult.vue
@@ -1,16 +1,10 @@
 <template>
   <v-container fluid style="min-height: 100vh; padding: 0 10vw;">
-    <h2 class="red--text">本語料非來自隨機抽樣之常模,分析結果數值僅提供參考,不適合作為幼兒語言發展篩檢或診斷之標準</h2>
+    <h2 class="red--text">{{warnMessage}}</h2>
     <v-row>
       <!-- Analysis result table -->
       <v-col cols="12" md="8">
-        <v-data-table
-          v-if="items"
-          :headers="headers"
-          :items="items"
-          hide-default-footer
-          disable-pagination
-        ></v-data-table>
+        <slot></slot>
       </v-col>
       <!-- buttons -->
       <v-col cols="12" md="4">
@@ -29,12 +23,7 @@
     </v-row>
     <!-- padding = = -->
     <br v-for="n in 15" />
-    <UiAlert
-      v-model="downloadAlert"
-      title="提醒"
-      subtitle="本語料非來自隨機抽樣之常模,分析結果數值僅提供參考,不適合作為幼兒語言發展篩檢或診斷之標準"
-      @ok="download"
-    ></UiAlert>
+    <UiAlert v-model="downloadAlert" title="提醒" :subtitle="warnMessage" @ok="download"></UiAlert>
     <UiAlert v-model="alert" title="確定要重新分析嗎？" subtitle="您目前的分析資料將不會被保留。" @ok="$emit('restart')"></UiAlert>
   </v-container>
 </template>
@@ -45,75 +34,24 @@ import UiAlert from "@/components/ui-alert";
 
 export default {
   name: "AnalysisResult",
-
   components: {
     UiAlert,
   },
-
   props: {
-    data: {
-      type: Object,
-    },
     filename: {
       type: String,
       required: true,
     },
   },
-
-  computed: {
-    items() {
-      if (this.data) {
-        var temp = [];
-        for (const [key, value] of Object.entries(this.indicator)) {
-          temp.push({
-            name: value,
-            avg: Math.round((this.data[key][0] + Number.EPSILON) * 100) / 100,
-            sd: Math.round((this.data[key][1] + Number.EPSILON) * 100) / 100,
-            size: this.data[key][2],
-          });
-        }
-        return temp;
-      }
-      return null;
-    },
-  },
-
   data() {
     return {
       alert: false,
       downloadAlert: false,
-      headers: [
-        {
-          text: "語言指標",
-          align: "start",
-          value: "name",
-          class: "text--primary subtitle-1 font-weight-bold",
-          width: "30%",
-        },
-        {
-          text: "平均",
-          value: "avg",
-          class: "text--primary subtitle-1 font-weight-bold",
-          width: "25%",
-        },
-        {
-          text: "標準差",
-          value: "sd",
-          class: "text--primary subtitle-1 font-weight-bold",
-          width: "25%",
-        },
-        {
-          text: "有效筆數",
-          value: "size",
-          class: "text--primary subtitle-1 font-weight-bold",
-          width: "20%",
-        },
-      ],
-      indicator: json.reindicator,
       cache: null,
+      warnMessage:
+        "本語料非來自隨機抽樣之常模,分析結果數值僅提供參考,不適合作為幼兒語言發展篩檢或診斷之標準",
     };
   },
-
   methods: {
     async download() {
       if (!this.cache) {
--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -2,32 +2,27 @@
   <v-stepper v-model="step">
     <v-stepper-header>
       <v-divider></v-divider>
-
       <v-stepper-step :complete="step > 1" step="1">選擇分析條件</v-stepper-step>
-
       <v-divider></v-divider>
-
       <v-stepper-step :complete="step > 2" step="2">查看分析結果</v-stepper-step>
-
       <v-divider></v-divider>
     </v-stepper-header>
-
     <v-stepper-items>
       <v-stepper-content step="1">
         <FileFilter @next="step1" :data="data" :loading="loading" :fileMode="file"></FileFilter>
       </v-stepper-content>
-
       <v-stepper-content step="2">
-        <AnalysisResult
-          @restart="restart"
-          @back="step = step-1"
-          :filename="filename"
-          :indicator="data.indicator"
-          :data="items"
-        ></AnalysisResult>
+        <AnalysisResult @restart="restart" @back="step = step-1" :filename="filename">
+          <v-data-table
+            v-if="tableItems"
+            :headers="headers"
+            :items="tableItems"
+            hide-default-footer
+            disable-pagination
+          ></v-data-table>
+        </AnalysisResult>
       </v-stepper-content>
     </v-stepper-items>
-
     <v-snackbar v-model="snackbar" color="error">{{snackbarText}}</v-snackbar>
   </v-stepper>
 </template>
@@ -35,6 +30,7 @@
 <script>
 import FileFilter from "@/components/FileFilter";
 import AnalysisResult from "@/components/AnalysisResult";
+import charArgs from "./step1.json";
 
 export default {
   name: "Analysis",
@@ -59,6 +55,33 @@ export default {
       },
       snackbar: false,
       snackbarText: "",
+      headers: [
+        {
+          text: "語言指標",
+          align: "start",
+          value: "name",
+          class: "text--primary subtitle-1 font-weight-bold",
+          width: "30%",
+        },
+        {
+          text: "平均",
+          value: "avg",
+          class: "text--primary subtitle-1 font-weight-bold",
+          width: "25%",
+        },
+        {
+          text: "標準差",
+          value: "sd",
+          class: "text--primary subtitle-1 font-weight-bold",
+          width: "25%",
+        },
+        {
+          text: "有效筆數",
+          value: "size",
+          class: "text--primary subtitle-1 font-weight-bold",
+          width: "20%",
+        },
+      ],
     };
   },
 
@@ -74,8 +97,27 @@ export default {
       this.restart();
       return false;
     },
+    tableItems() {
+      // get analysis result
+      if (this.items) {
+        var temp = [];
+        // calculate avg and std for each indicator
+        for (const [key, value] of Object.entries(chatArgs.reindicator)) {
+          if (key in this.items) {
+            temp.push({
+              name: value,
+              avg:
+                Math.round((this.items[key][0] + Number.EPSILON) * 100) / 100,
+              sd: Math.round((this.items[key][1] + Number.EPSILON) * 100) / 100,
+              size: this.items[key][2],
+            });
+          }
+        }
+        return temp;
+      }
+      return null;
+    },
   },
-
   methods: {
     step1(data) {
       this.loading = true;
@@ -93,7 +135,7 @@ export default {
         .catch((err) => {
           console.log(err);
           if (err.response && err.response.status === 404) {
-            // not file filtered
+            // no file filtered
             this.snackbarText = "語料庫無符合搜尋條件之語料";
           } else {
             // unhandled errors
