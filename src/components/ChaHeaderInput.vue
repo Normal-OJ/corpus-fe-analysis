81c84d86eed8d66937945592019c748404a09928
Uier <ap9940506@gmail.com>
2020-09-17 01:12:32 +0800

    adjust layout & add text to selection items

--- src/components/ChaHeaderInput.vue
+++ src/components/ChaHeaderInput.vue
@@ -1,8 +1,18 @@
 <template>
   <v-container>
     <h1>手動輸入表頭 (Header)</h1>
+
+    <h2>Header 預覽</h2>
+    <v-textarea
+      v-model="header"
+      class="flex-grow-1 pa-1 header-text"
+      :style="{ fontFamily: 'monospace' }"
+      outlined
+      @keydown.tab.prevent="appendTabToHeader($event)"
+    />
+
     <v-row>
-      <v-col cols="6">
+      <v-col col="12" md="6">
         <h2>編輯 ID</h2>
         <v-select
           ref="speakerSelect"
@@ -11,84 +21,115 @@
           :items="speakerNames"
           @change="changeSpeaker"
         />
+      </v-col>
+      <v-col col="12" md="6">
         <v-row justify="space-around">
           <v-btn color="red" dark @click="deleteId()">刪除 ID</v-btn>
           <v-btn color="primary" @click="addId()">新增 ID</v-btn>
           <v-btn color="primary" @click="getHeader()">更新表頭</v-btn>
         </v-row>
-        <!-- i can not bind the data.. -->
-        <!-- <SpeakerInput v-bind.sync="speaker"></SpeakerInput> -->
-        <v-container>
-          <h3>說話者名稱 (Name code)</h3>
-          <v-text-field v-model="selectedName" outlined placeholder="例如： CHI, MOT..." dense />
-
-          <h3>完整名稱 (Optional speaker name)</h3>
-          <v-text-field v-model="speaker.name" outlined dense />
-
-          <h3>語言 (Language)</h3>
-          <v-text-field v-model="speaker.language" placeholder="例如： zho" outlined dense />
-
-          <h3>語料名稱 (Corpus name)</h3>
-          <v-text-field v-model="speaker.corpus" outlined dense />
-
-          <h3>年齡 (Age)</h3>
-          <div>月、日不足兩碼請補 0，例如： 05、07</div>
-          <v-row>
-            <v-col>
-              <v-text-field outlined v-model="speaker.age.y" placeholder="年" dense />
-            </v-col>
-            <v-col>
-              <v-text-field outlined v-model="speaker.age.m" placeholder="月" dense />
-            </v-col>
-            <v-col>
-              <v-text-field outlined v-model="speaker.age.d" placeholder="日" dense />
-            </v-col>
-          </v-row>
-
-          <h3>性別 (Sex)</h3>
-          <v-radio-group row v-model="speaker.sex">
-            <v-radio
-              v-for="(_sex, value) in sexChoices"
-              :key="value"
-              class="mb-3"
-              :label="value"
-              :value="_sex"
-            ></v-radio>
-          </v-radio-group>
-
-          <v-row>
-            <v-col>
-              <h3>種族 (Race)</h3>
-              <v-select outlined v-model="speaker.race" :items="raceChoices" dense></v-select>
-            </v-col>
-            <v-col>
-              <h3>社經地位 (SES)</h3>
-              <v-select outlined v-model="speaker.SES" :items="SESChoices" dense></v-select>
-            </v-col>
-          </v-row>
-
-          <h3>組別 (Group)</h3>
-          <v-text-field v-model="speaker.group" outlined dense />
-
-          <h3>角色 (Role)</h3>
-          <v-select v-model="speaker.role" :items="roleChoices" outlined dense></v-select>
-
-          <h3>教育程度 (Education)</h3>
-          <v-text-field v-model="speaker.education" outlined dense />
-
-          <h3>其他所需資訊 (Custom field)</h3>
-          <v-text-field v-model="speaker.customField" outlined dense />
-        </v-container>
       </v-col>
-      <v-col class="d-flex flex-column" cols="6">
-        <h2>Header 預覽</h2>
-        <v-textarea
-          v-model="header"
-          class="flex-grow-1 pa-1 header-text"
-          :style="{ fontFamily: 'monospace' }"
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="6">
+        <h3>說話者名稱 (Name code)</h3>
+        <v-text-field
+          v-model="selectedName"
           outlined
-          @keydown.tab.prevent="appendTabToHeader($event)"
+          placeholder="例如： CHI, MOT..."
+          dense
         />
+
+        <h3>完整名稱 (Optional speaker name)</h3>
+        <v-text-field v-model="speaker.name" outlined dense />
+
+        <h3>語言 (Language)</h3>
+        <v-combobox
+          v-model="speaker.language"
+          placeholder="例如： zho"
+          :items="languageChoices"
+          outlined
+          dense
+        />
+
+        <h3>語料名稱 (Corpus name)</h3>
+        <v-text-field v-model="speaker.corpus" outlined dense />
+
+        <h3>年齡 (Age)</h3>
+        <div>月、日不足兩碼請補 0，例如： 05、07</div>
+        <v-row>
+          <v-col>
+            <v-text-field
+              outlined
+              v-model="speaker.age.y"
+              placeholder="年"
+              dense
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              outlined
+              v-model="speaker.age.m"
+              placeholder="月"
+              dense
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              outlined
+              v-model="speaker.age.d"
+              placeholder="日"
+              dense
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="12" md="6">
+        <h3>性別 (Sex)</h3>
+        <v-radio-group row v-model="speaker.sex">
+          <v-radio
+            v-for="(_sex, value) in sexChoices"
+            :key="value"
+            class="mb-3"
+            :label="value"
+            :value="_sex"
+          ></v-radio>
+        </v-radio-group>
+
+        <v-row>
+          <v-col>
+            <h3>種族 (Race)</h3>
+            <v-select
+              outlined
+              v-model="speaker.race"
+              :items="raceChoices"
+              dense
+            />
+          </v-col>
+          <v-col>
+            <h3>社經地位 (SES)</h3>
+            <v-select
+              outlined
+              v-model="speaker.SES"
+              :items="SESChoices"
+              dense
+            />
+          </v-col>
+        </v-row>
+
+        <h3>組別 (Group)</h3>
+        <v-text-field v-model="speaker.group" outlined dense />
+
+        <h3>角色 (Role)</h3>
+        <v-select v-model="speaker.role" :items="roleChoices" outlined dense />
+
+        <h3>教育程度 (Education)</h3>
+        <v-text-field v-model="speaker.education" outlined dense />
+
+        <h3>其他所需資訊 (Custom field)</h3>
+        <v-text-field v-model="speaker.customField" outlined dense />
       </v-col>
     </v-row>
   </v-container>
@@ -98,6 +139,7 @@
 // import SpeakerInput from "@/components/SpeakerInput";
 import {
   Speaker,
+  languageChoices,
   roleChoices,
   raceChoices,
   SESChoices,
@@ -117,6 +159,7 @@ export default {
     speaker: new Speaker(),
     selectedName: "",
     oldNameCode: "",
+    languageChoices,
     roleChoices,
     raceChoices,
     SESChoices,
