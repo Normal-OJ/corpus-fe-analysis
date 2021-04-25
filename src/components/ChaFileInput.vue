4b52941da8889df79b2e74d1564f7e3f784ee7e3
Uier <ap9940506@gmail.com>
2021-04-26 01:50:46 +0800

    fix(chafileinput): since terms entry will also trigger the agree checkbox, so I do some magic
    good luck

--- src/components/ChaFileInput.vue
+++ src/components/ChaFileInput.vue
@@ -4,22 +4,27 @@
     <v-form ref="form">
       <v-file-input
         v-model="file"
-        class="flex-grow-1 ma-2"
+        class="my-2"
         accept=".cha"
         label="請選擇你的 cha 檔"
         :rules="[v => !!v || '請上傳檔案']"
       ></v-file-input>
-      <v-checkbox v-model="isAgreed" :rules="[v => v || '您必須同意條款']">
+      <v-checkbox v-model="isAgreed" :rules="[v => v]">
         <template v-slot:label>
           <div>
             我已閱讀並同意
-            <span class="info--text" @click.stop="dialog = true">
-              語料上傳使用條款
-            </span>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <span class="info--text" @click.stop="open" v-on="on">
+                  語料上傳使用條款
+                </span>
+              </template>
+              點擊以檢視條款
+            </v-tooltip>
           </div>
         </template>
       </v-checkbox>
-      <v-btn class="flex-shrink-1" color="primary" @click="submit">上傳檔案</v-btn>
+      <v-btn class="mt-4" color="primary" @click="submit">上傳檔案</v-btn>
     </v-form>
     <v-dialog v-model="dialog" width="50%">
       <v-card>
@@ -27,10 +32,15 @@
           <v-toolbar-title>語料上傳使用條款</v-toolbar-title>
           <v-spacer />
           <v-toolbar-items>
-            <v-btn icon @click="dialog = false"><v-icon>mdi-close</v-icon></v-btn>
+            <v-btn icon @click="deny"><v-icon>mdi-close</v-icon></v-btn>
           </v-toolbar-items>
         </v-toolbar>
         <v-card-text class="mt-4 text--primary text-body-1" v-text="terms['upload']" />
+        <v-card-actions>
+          <v-spacer />
+          <v-btn text color="primary" @click="accept">同意</v-btn>
+          <v-btn text color="error" @click="deny">不同意</v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
   </v-container>
@@ -52,6 +62,18 @@ export default {
       if (this.$refs.form.validate()) {
         $emit('upload-file', file);
       }
+    },
+    open() {
+      this.dialog = true;
+      this.isAgreed = !this.isAgreed;
+    },
+    accept() {
+      this.dialog = false;
+      this.isAgreed = true;
+    },
+    deny() {
+      this.dialog = false;
+      this.isAgreed = false;
     },
   },
 };
